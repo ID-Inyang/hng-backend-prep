8b0865c111bfb595ee774d25c14000ebfc41b1a5
ID-Inyang <bobbyinyang9@gmail.com>
2026-06-16 05:12:29 +0100

    Add user model with schema definition for username, password, and email

---
+++ intro-to-backend/backend/src/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            minLength: 1,
+            maxlength: 20
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 6,
+            maxlength: 50
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model("User", userSchema)
