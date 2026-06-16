e2c7060dff89c3f9e98ad67d9f7437736a9ed42a
ID-Inyang <bobbyinyang9@gmail.com>
2026-06-16 04:54:39 +0100

    Set up server entry point and database connection; refactor package scripts

--- intro-to-backend/backend/src/config/database.js
+++ intro-to-backend/backend/src/config/database.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import { configDotenv } from 'dotenv';
 
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
-        console.log(`\n Database connected successfully ${connectionInstance.connection.host}`)
+        console.log(`\nDatabase connected successfully ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("Database connection failed", error);
         process.exit(1)
