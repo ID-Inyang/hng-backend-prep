d4af6f7ee7707a445c23e032d47b809204915997
ID-Inyang <bobbyinyang9@gmail.com>
2026-06-16 05:49:09 +0100

    Refactor registerUser function to improve user existence check and response handling

--- intro-to-backend/backend/src/controllers/user.controller.js
+++ intro-to-backend/backend/src/controllers/user.controller.js
@@ -11,8 +11,30 @@ const registerUser = async (req, res) => {
 
         // check if user exist already
 
-        const existing = await User.findOne({ email: email.toLowerCase() })
-    } catch (error) {
+        const existing = await User.findOne({ email: email.toLowerCase() });
+        if (existing) {
+            return res.status(400).json({ message: "user already exists!" })
+        }
 
+        // Create user
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password,
+            loggedIn: false,
+        });
+
+        res.status(201).json({
+            message: "User registered",
+            user: { id: user._id, email: user.email, username: username }
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message })
     }
+}
+
+export {
+    registerUser
 }
