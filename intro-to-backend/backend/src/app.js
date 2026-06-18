82c48376eb88921221bc56f749909b90c25f83a9
ID-Inyang <bobbyinyang9@gmail.com>
2026-06-18 20:19:20 +0100

    Remove example route comment from app.js

--- intro-to-backend/backend/src/app.js
+++ intro-to-backend/backend/src/app.js
@@ -10,6 +10,4 @@ import userRouter from './routes/user.route.js'
 // routes declaration
 app.use("/api/v1/users", userRouter)
 
-// example route: http://localhost:4000/api/v1/users/register
-
 export default app;
